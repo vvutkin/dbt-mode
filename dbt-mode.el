7c736bd47ee0323a862e9cab154b1d4d88e47a96
Richard Fulop <richie@freemarkethealth.com>
2022-06-28 14:47:49 -0400

    Add commands for buffer-specific compile lookup, build, run, test

--- dbt-mode.el
+++ dbt-mode.el
@@ -13,49 +13,73 @@
 ;;; Code:
 
 (defgroup dbt nil
-  "Interact with sql databases using data build tool"
+  "Interact with sql databases using data build tool."
   :prefix "dbt-"
   :group 'sql)
 
-(defcustom dbt-project-root nil
-  "Set the root path for the dbt project"
-  :group 'dbt)
-
 (defun dbt-run-buffer ()
-    "Run model. If specified, run `model-name`
-     otherwise, run the model in the current buffer"
+    "Run model from the current buffer."
     (interactive)
-    (let ((model (file-name-base buffer-file-name))
-          (proj-directory (file-name-directory buffer-file-name)))
+    (let ((model (file-name-base buffer-file-name)))
       (async-shell-command (format "dbt run --select %s" model))))
 
 (defun dbt-run ()
-  "Run the entire dbt project"
+  "Run the entire dbt project."
   (interactive)
   (async-shell-command "dbt run"))
 
 (defun dbt-test ()
-  "Test the entire dbt project"
+  "Test the entire dbt project."
   (interactive)
   (async-shell-command "dbt test"))
 
 (defun dbt-format-buffer ()
+  "Call dbt-format shell function on the current buffer's file location."
   (interactive)
   (save-buffer)
-  (set 'command-text (format "autoload dbt-format; dbt-format --replace -f \"%s\"" (buffer-file-name (window-buffer (minibuffer-selected-window)))))
-  (message "Running shell command \"%s\"" command-text)
-  (message "Command returned with: %s" (shell-command-to-string command-text))
+  (let ((command-text (format "autoload dbt-format; dbt-format --replace -f \"%s\"" (buffer-file-name (window-buffer (minibuffer-selected-window))))))
+    (message "Running shell command \"%s\"" command-text)
+    (message "Command returned with: %s" (shell-command-to-string command-text)))
   (save-buffer))
 
+(defun dbt-build-buffer ()
+    "Run model from the current buffer."
+    (interactive)
+    (let ((model (file-name-base buffer-file-name)))
+      (async-shell-command (format "dbt build --select %s" model))))
+
+(defun dbt-build ()
+  "Call dbt build on project in the current directory."
+  (interactive)
+  (async-shell-command "dbt build"))
+
+(defun dbt-get-compiled-version (file-name)
+    "Get the path to the compiled version of the file.
+FILE-NAME: the path to the model"
+    (let* ((git-root (replace-regexp-in-string "\n" "" (shell-command-to-string "pushd ~/workspace/dbt-analytics; git rev-parse --show-toplevel")))
+           (file-name-regex (concat "^" (file-name-nondirectory file-name) "$")))
+      (car (directory-files-recursively (concat git-root "/target/compiled/") file-name-regex ))))
+
+(defun dbt-open-compiled ()
+  "Open the compiled version of the current buffer file."
+  (interactive)
+  (find-file (dbt-get-compiled-version buffer-file-name)))
+
+(defun dbt-compile ()
+  "Call dbt compile on project in the current directory."
+  (interactive)
+  (async-shell-command "dbt compile"))
+
+;;;###autoload
 (define-minor-mode dbt-mode
-  "Toggle dbt mode, a global minor mode"
+  "Toggle dbt mode, a local minor mode."
   :global nil
   :group 'dbt
-  :interactive `(sql-mode)
-  :lighter " dbt"
-  :keymap '(([]))
-  (unless dbt-project-root (setq dbt-project-root (file-name-directory buffer-file-name))))
+  :lighter " dbt")
 
-(provide 'dbt)
+;;;###autoload
+(add-hook 'sql-mode-hook 'dbt-mode)
+
+(provide 'dbt-mode)
 
 ;;; dbt-mode.el ends here
